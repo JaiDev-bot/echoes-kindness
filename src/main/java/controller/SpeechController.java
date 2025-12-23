1493e8a558d289d1091b6a435261547422a09884
Jaiane <jaianesoares700@gmail.com>
2025-12-23 10:31:28 -0300

    feat: feito método GET 'ouvir'

---
+++ src/main/java/controller/SpeechController.java
@@ -0,0 +1,35 @@
+package controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.SpeechService;
+
+@RestController
+@RequestMapping("api/acessibilidade")
+public class SpeechController {
+
+    private final SpeechService speechService;
+
+    public SpeechController(SpeechService speechService) {
+        this.speechService = speechService;
+    }
+
+    @GetMapping("/ouvir")
+    public String iniciarOuvinte() {
+
+        new Thread(() -> {
+            try {
+                speechService.transcricaoEmTempoReal();
+            } catch (Exception e) {
+                System.err.println("Erro na transcrição: " + e.getMessage());
+            }
+        }).start();
+
+        return "Microfone ativado! Acompanhe a transcrição no console do IntelliJ.";
+    }
+}
+
+
