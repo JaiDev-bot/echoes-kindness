f4e23f8add65ea6b5cc350aaea99339b79145d60
Jaiane <jaianesoares700@gmail.com>
2025-12-22 21:53:09 -0300

    first commit

---
+++ src/main/java/jaiane/com/Echoes/EchoesApplication.java
@@ -0,0 +1,13 @@
+package jaiane.com.Echoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jaiane/com/Echoes/EchoesApplicationTests.java
@@ -0,0 +1,13 @@
+package jaiane.com.Echoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
